4cce36f657d3a1b2fd6b5da7095467e84929f92a
Kirill <kirill.parhomenko@gmail.com>
2019-08-29 20:07:41 +0500

    ADD: Descending Order task

---
+++ Tasks/Descending Order/task.py
@@ -0,0 +1,22 @@
+"""
+Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+
+Input: 21445 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 1254859723 Output: 9875543221
+
+Link: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/python
+"""
+
+
+def Descending_Order(num):
+    num = sorted(str(num), reverse=True)
+    num = "".join(str(x) for x in num)
+    return int(num)
+
+
+Descending_Order(145263)
